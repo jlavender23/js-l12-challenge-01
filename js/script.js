de1b19c378c27fd5cfe369d3702e412155051062
Jordan Lavender <jel2ub@virginia.edu>
2022-09-18 16:16:30 -0400

    fetched api data

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
+const getImage = async function () {
+    const res = await fetch ("https://picsum.photos/v2/list?limit=100");
+    //this enables communication btw my program and API
+    const images = await res.json(); 
+    //parsed data captured in res using json
+    console.log(images);
+};
+getImage();
